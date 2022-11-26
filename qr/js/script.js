17ee8ed8fc2eea5158f44bd94f11bb96038f7217
SkylerNv <adamasnoctua@github.com>
2022-11-26 16:32:32 +0300

    Initial commit

---
+++ qr/js/script.js
@@ -0,0 +1,22 @@
+var qrcode = new QRCode(document.getElementById("qr-code"), {
+    width: 256,
+    height: 256,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
+
+function makeCode(){
+    let textInput = document.getElementById("text-input").value;
+    if(!textInput) return;
+    qrcode.makeCode(textInput);
+}
+
+let textInput = document.getElementById("text-input");
+
+textInput.addEventListener('keypress', event => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("generate-button").click();
+    }
+})
